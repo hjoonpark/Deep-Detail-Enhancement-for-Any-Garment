e7dcd3946171bf6801b324a4bee2d30de3b1e99a
hjoonpark <hjoonpark.us@gmail.com>
2023-05-10 22:07:39 -0500

    normal map to obj done

--- network_train_and_run/run_teGeometry_Opt.sh
+++ network_train_and_run/run_teGeometry_Opt.sh
@@ -1,3 +1,5 @@
 clear;
-nohup python teGeometry_Opt.py --folder "fear" --start-idx 0 --end-idx 110 --cuda-idx 0 > output_ours/test/log1.txt &
-nohup python teGeometry_Opt.py --folder "fear" --start-idx 110 --end-idx 220 --cuda-idx 0 > output_ours/test/log2.txt &
+nohup python teGeometry_Opt.py --folder "fear" --start-idx 0 --end-idx 52 --cuda-idx 0 > output_ours/test/log_euc5.txt &
+nohup python teGeometry_Opt.py --folder "fear" --start-idx 52 --end-idx 104 --cuda-idx 0 > output_ours/test/log_euc6.txt &
+nohup python teGeometry_Opt.py --folder "fear" --start-idx 104 --end-idx 156 --cuda-idx 1 > output_ours/test/log_euc7.txt &
+nohup python teGeometry_Opt.py --folder "fear" --start-idx 156 --end-idx 220 --cuda-idx 1 > output_ours/test/log_euc8.txt &
